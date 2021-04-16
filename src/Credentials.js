137fe2869d0b8d5e60c7fe402406503d2c09d0eb
Elizabeth O. Segun-Ajiboye <66387727+ElizabethdevQueen@users.noreply.github.com>
2021-04-16 17:35:22 +0100

    working on the extras

---
+++ src/Credentials.js
@@ -0,0 +1,9 @@
+const Credentials = () => {
+
+    return {
+        ClientId: "3c99a50cc4914fbd8348a5ddfbdf7225",
+    ClientSecret: "d1434e6fb0004b1bb2ed5c654fb31b20"
+    }
+}
+
+export { Credentials };
